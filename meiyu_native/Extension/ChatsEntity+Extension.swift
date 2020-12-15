7ee832d5bf8f7d84e143fa05f16e6eeb6aa179bf
bobMurai <murai.sho@gmail.com>
2020-12-16 03:14:17 +0900

    create ChatsEntity+Extension

---
+++ meiyu_native/Extension/ChatsEntity+Extension.swift
@@ -0,0 +1,103 @@
+//
+//  ChatsEntity+Extension.swift
+//  meiyu_native
+//
+//  Created by 村井翔 on 2020/12/16.
+//
+
+
+import CoreData
+import SwiftUI
+
+extension ChatsEntity {
+    
+    static func create(in managedObjectContext: NSManagedObjectContext,
+                       id: String,
+                       friend_relation_id: String,
+                       user_id: String,
+                       content: String,
+                       created_at: Date,
+                       updated_at: Date){
+        let chat = self.init(context: managedObjectContext)
+        chat.id = id
+        chat.friend_relation_id = friend_relation_id
+        chat.user_id = user_id
+        chat.content = content
+        chat.created_at = created_at
+        chat.updated_at = updated_at
+        
+        do {
+            try  managedObjectContext.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+
+    
+//
+//    enum Category: Int16 {
+//        case ImpUrg_1st     // Important & Urgent (第Ⅰ領域）
+//        case ImpNUrg_2nd    // Important & Not Urgent (第Ⅱ領域）
+//        case NImpUrg_3rd    // Not Important & Urgent（第Ⅲ領域）
+//        case NImpNUrg_4th   // Not Important & Not Urgent（第Ⅳ領域）
+//
+//        func toString() -> String {
+//            switch self {
+//            case .ImpUrg_1st:
+//                return "重要かつ緊急"
+//            case .ImpNUrg_2nd:
+//                return "重要だが緊急ではない"
+//            case .NImpUrg_3rd:
+//                return "重要でないが緊急"
+//            case .NImpNUrg_4th:
+//                return "重要でも緊急でもない"
+//            }
+//        }
+//        func image() -> String {
+//            switch self {
+//            case .ImpUrg_1st:
+//                return "flame"
+//            case .ImpNUrg_2nd:
+//                return "tortoise.fill"
+//            case .NImpUrg_3rd:
+//                return "alarm"
+//            case .NImpNUrg_4th:
+//                return "tv.music.note"
+//            }
+//        }
+//        func color() -> Color {
+//            switch self {
+//            case .ImpUrg_1st:
+//                return .tRed
+//            case .ImpNUrg_2nd:
+//                return .tBlue
+//            case .NImpUrg_3rd:
+//                return .tGreen
+//            case .NImpNUrg_4th:
+//                return .tYellow
+//            }
+//        }
+//    }
+//
+//    enum State: Int16 {
+//        case todo
+//        case done
+//    }
+//
+//    static func count(in managedObjectContext: NSManagedObjectContext,
+//                      category: Category) -> Int {
+//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TodoEntity")
+//        fetchRequest.predicate = NSPredicate(format: "category == \(category.rawValue)")
+//
+//
+//        do {
+//            let count = try managedObjectContext.count(for: fetchRequest)
+//            return count
+//        } catch  {
+//            print("Error: \(error.localizedDescription)")
+//            return 0
+//        }
+//    }
+//}
+//
